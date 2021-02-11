2680c497e57efb38a80909d529c71d76ef1aeb72
thisIsShikhar <shikharali13@gmail.com>
2021-02-11 19:41:42 +0600

    updated

--- btre project/btre/settings.py
+++ btre project/btre/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,3 +138,11 @@ STATICFILES_DIRS= [
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+# Messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+
+    messages.ERROR: 'danger'
+}
